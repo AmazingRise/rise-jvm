5654d15e0f33abcab418a2dfb6d5be6e4bac0619
AmazingRise <8315221+AmazingRise@users.noreply.github.com>
2022-04-05 00:07:49 +0800

    Add demo

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ var worker = new Worker('worker.js')
 
 worker.onmessage = function (message) {
     document.getElementById("progress").style.visibility = "hidden";
-    document.getElementById("console").innerText = message.data;
+    document.getElementById("text").innerText = message.data + "\nProcess exited normally.";
+    document.getElementById("file-selector").value = "";
 };
+
+const exec = function(file) {
+    document.getElementById("progress").style.visibility = "visible";
+    document.getElementById("text").innerText = "Please wait while executing...";
+    worker.postMessage(file);
+}
 
 const fileSelector = document.getElementById('file-selector');
 fileSelector.addEventListener('change', (event) => {
-    // event.target.files[0]
     if (event.target.files.length < 1) {
         return
     }
-    document.getElementById("progress").style.visibility = "visible";
-    document.getElementById("console").innerText = "Please wait while executing...";
-    worker.postMessage(event.target.files[0]);
+    exec(event.target.files[0]);
 });
 
-
-
+const load_demo = function () {
+    fetch("Main.class").then(async (res)=>{
+        worker.postMessage(await res.blob());
+    })
+}
